161c69e302cf126bd8b1a547d975e72eae1a5d27
Jalal azimi <jalalazimi@gmail.com>
2019-06-06 11:22:34 +0100

    feat: create size for buttons and update storybook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -11,29 +11,53 @@ import {
 import '../dist/css/button.css';
 import fontawesome from "@fortawesome/fontawesome-free/css/all.css";
 
-const colors = ['blue', 'purple', 'red', 'orange', 'green', 'white', 'black']
+const colors = ['blue', 'purple', 'red', 'orange', 'green', 'white', 'black'];
+const sizes = ['xxsmall', 'xsmall', 'small', 'compact', 'large', 'xlarge', 'xxlarge'];
 
-storiesOf('Simple Buttons', module)
-  .add('Default', () => `${colors.map(color => `<button class="button -${color}">button</button>`).join(' ')}
-   <button class="button -blue"><i class="fas fa-angle-left icon-left"></i>button</button> <button class="button -purple">button<i class="fas fa-angle-right icon-right"></i></button> <button class="button -red"><i class="fas fa-cog"></i></button>`)
-  .add('Outline', () => `${colors.map(color => `<button class="button -outline -${color}">button</button>`).join(' ')}
-   <button class="button -outline -blue"><i class="fas fa-angle-left icon-left"></i>button</button> <button class="button -outline -purple">button<i class="fas fa-angle-right icon-right"></i></button> <button class="button -outline -red"><i class="fas fa-cog"></i></button>`)
-  .add('Gradient', () => `${colors.map(color => `<button class="button -gradient -${color}">button</button>`).join(' ')}
-   <button class="button -gradient -blue"><i class="fas fa-angle-left icon-left"></i>button</button> <button class="button -gradient -purple">button<i class="fas fa-angle-right icon-right"></i></button> <button class="button -gradient -red"><i class="fas fa-cog"></i></button>`)
+const Button = (className, children = "button") => `<button class="button ${className}">${children}</button>`
 
-storiesOf('Pill Buttons', module)
-  .add('Default', () => `${colors.map(color => `<button class="button -pill -${color}">button</button>`).join(' ')}
-   <button class="button -pill -blue"><i class="fas fa-angle-left icon-left"></i>button</button> <button class="button -pill -purple">button<i class="fas fa-angle-right icon-right"></i></button> <button class="button -pill -red"><i class="fas fa-cog"></i></button>`)
-  .add('Outline', () => `${colors.map(color => `<button class="button -pill -outline -${color}">button</button>`).join(' ')}
-   <button class="button -pill -outline -blue"><i class="fas fa-angle-left icon-left"></i>button</button> <button class="button -pill -outline -purple">button<i class="fas fa-angle-right icon-right"></i></button> <button class="button -pill -outline -red"><i class="fas fa-cog"></i></button>`)
-  .add('Gradient', () => `${colors.map(color => `<button class="button -pill -gradient -${color}">button</button>`).join(' ')}
-   <button class="button -pill -gradient -blue"><i class="fas fa-angle-left icon-left"></i>button</button> <button class="button -pill -gradient -purple">button<i class="fas fa-angle-right icon-right"></i></button> <button class="button -pill -gradient -red"><i class="fas fa-cog"></i></button>`)
+storiesOf('Simple', module)
+  .add('Default', () => `${colors.map(color => Button(`-${color}`)).join(' ')}
+    ${Button('-blue', '<i class="fas fa-angle-left icon-left"></i>button')}
+    ${Button('-purple', 'button<i class="fas fa-angle-right icon-right"></i>')}
+    ${Button('-red', '<i class="fas fa-cog"></i>')}`)
+  .add('Outline', () => `${colors.map(color => Button(`-outline -${color}`)).join(' ')}
+    ${Button('-blue -outline', '<i class="fas fa-angle-left icon-left"></i>button')}
+    ${Button('-purple -outline', 'button<i class="fas fa-angle-right icon-right"></i>')}
+    ${Button('-red -outline', '<i class="fas fa-cog"></i>')}`)
+  .add('Gradient', () => `${colors.map(color => Button(`-gradient -${color}`)).join(' ')}
+    ${Button('-blue -gradient', '<i class="fas fa-angle-left icon-left"></i>button')}
+    ${Button('-purple -gradient', 'button<i class="fas fa-angle-right icon-right"></i>')}
+    ${Button('-red -gradient', '<i class="fas fa-cog"></i>')}`)
+
+storiesOf('Pill', module)
+  .add('Default', () => `${colors.map(color => Button(`-pill -${color}`)).join(' ')}
+    ${Button('-blue -pill', '<i class="fas fa-angle-left icon-left"></i>button')}
+    ${Button('-purple -pill', 'button<i class="fas fa-angle-right icon-right"></i>')}
+    ${Button('-red -pill', '<i class="fas fa-cog"></i>')}`)
+  .add('Outline', () => `${colors.map(color => Button(`-pill -outline -${color}`)).join(' ')}
+    ${Button('-blue -pill -outline', '<i class="fas fa-angle-left icon-left"></i>button')}
+    ${Button('-purple -pill -outline', 'button<i class="fas fa-angle-right icon-right"></i>')}
+    ${Button('-red -pill -outline', '<i class="fas fa-cog"></i>')}`)
+  .add('Gradient', () => `${colors.map(color => Button(`-pill -${color}`)).join(' ')}
+    ${Button('-blue -pill -gradient', '<i class="fas fa-angle-left icon-left"></i>button')}
+    ${Button('-purple -pill -gradient', 'button<i class="fas fa-angle-right icon-right"></i>')}
+    ${Button('-red -pill -gradient', '<i class="fas fa-cog"></i>')}`)
+
+storiesOf('Rounded', module)
+  .add('Default', () => `${colors.map(color => Button(`-rounded -${color}`)).join(' ')}
+      ${Button('-blue -rounded', '<i class="fas fa-angle-left icon-left"></i>button')}
+      ${Button('-purple -rounded', 'button<i class="fas fa-angle-right icon-right"></i>')}
+      ${Button('-red -rounded', '<i class="fas fa-cog"></i>')}`)
+  .add('Outline', () => `${colors.map(color => Button(`-rounded -outline -${color}`)).join(' ')}
+      ${Button('-blue -rounded -outline', '<i class="fas fa-angle-left icon-left"></i>button')}
+      ${Button('-purple -rounded -outline', 'button<i class="fas fa-angle-right icon-right"></i>')}
+      ${Button('-red -rounded -outline', '<i class="fas fa-cog"></i>')}`)
+  .add('Gradient', () => `${colors.map(color => Button(`-rounded -${color}`)).join(' ')}
+      ${Button('-blue -rounded -gradient', '<i class="fas fa-angle-left icon-left"></i>button')}
+      ${Button('-purple -rounded -gradient', 'button<i class="fas fa-angle-right icon-right"></i>')}
+      ${Button('-red -rounded -gradient', '<i class="fas fa-cog"></i>')}`)
 
 
-storiesOf('Rounded Buttons', module)
-  .add('Default', () => `${colors.map(color => `<button class="button -rounded -${color}">button</button>`).join(' ')}
-   <button class="button -rounded -blue"><i class="fas fa-angle-left icon-left"></i>button</button> <button class="button -rounded -purple">button<i class="fas fa-angle-right icon-right"></i></button> <button class="button -rounded -red"><i class="fas fa-cog"></i></button>`)
-  .add('Outline', () => `${colors.map(color => `<button class="button -rounded -outline -${color}">button</button>`).join(' ')}
-   <button class="button -rounded -outline -blue"><i class="fas fa-angle-left icon-left"></i>button</button> <button class="button -rounded -outline -purple">button<i class="fas fa-angle-right icon-right"></i></button> <button class="button -rounded -outline -red"><i class="fas fa-cog"></i></button>`)
-  .add('Gradient', () => `${colors.map(color => `<button class="button -rounded -gradient -${color}">button</button>`).join(' ')}
-   <button class="button -rounded -gradient -blue"><i class="fas fa-angle-left icon-left"></i>button</button> <button class="button -rounded -gradient -purple">button<i class="fas fa-angle-right icon-right"></i></button> <button class="button -rounded -gradient -red"><i class="fas fa-cog"></i></button>`)
+storiesOf('Size', module)
+  .add('Default', () => sizes.map((size, index) => Button(`-${size}`, size)).join(' '))
