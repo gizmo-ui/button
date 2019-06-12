473ad63d278e2a895e27be44145d422ef8934f18
Jalal azimi <jalalazimi@gmail.com>
2019-06-12 01:24:16 +0100

    feat: update color and project demo

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,15 @@
 import { configure } from '@storybook/html';
+import { setDefaults } from "@storybook/addon-info";
+
+// addon-info
+setDefaults({
+  header: true, // Toggles display of header with component name and description
+  inline: true, // Displays info inline vs click button to view
+  source: true ,// Displays the source of story Component
+  brandTitle: "My custom storybook",
+  brandUrl: "https://example.com",
+  brandImage: "https://placehold.it/350x150"
+});
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,37 +1,46 @@
-import {
-  document,
-  console
-} from 'global';
-import {
-  storiesOf
-} from '@storybook/html';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { storiesOf } from '@storybook/react';
 import {
   withInfo
 } from '@storybook/addon-info';
+import renderHTML from 'react-render-html';
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { darcula } from "react-syntax-highlighter/dist/styles/prism";
+
 import '../dist/css/button.css';
-import fontawesome from "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 const colors = ['blue', 'purple', 'red', 'orange', 'green', 'white', 'black'];
 const sizes = ['xxsmall', 'xsmall', 'small', 'compact', 'large', 'xlarge', 'xxlarge'];
 
-const Button = (className, children = "button") => `<button class="button ${className}">${children}</button>`
+const Button = (className, children = "button") => renderHTML(` <button class="button ${className}">${children}</button> `);
+const Simple = (outline = false) => ([
+  colors.map(color => Button(`-${color} ${outline && '-outline'}`)),
+  Button(`-blue ${outline && '-outline'}`, '<i class="fas fa-angle-left icon-left"></i>button'),
+  Button(`-purple ${outline && '-outline'}`, 'button<i class="fas fa-angle-right icon-right"></i>'),
+  Button(`-red ${outline && '-outline'}`, '<i class="fas fa-cog"></i>'),
+  Button(`-white ${outline && '-outline'} -loading`, 'button'),
+  Button(`-black ${outline && '-outline'} -loading`, 'button'),
+  Button(`-green ${outline && '-outline'} -loading`, 'button')
+]);
 
 storiesOf('Simple', module)
-  .add('Default', () => `${colors.map(color => Button(`-${color}`)).join(' ')}
-    ${Button('-blue', '<i class="fas fa-angle-left icon-left"></i>button')}
-    ${Button('-purple', 'button<i class="fas fa-angle-right icon-right"></i>')}
-    ${Button('-red', '<i class="fas fa-cog"></i>')}
-    ${Button('-white -loading', 'button')}
-    ${Button('-black -loading', 'button')}
-    ${Button('-green -loading', 'button')}`)
-  .add('Outline', () => `${colors.map(color => Button(`-outline -${color}`)).join(' ')}
-    ${Button('-blue -outline', '<i class="fas fa-angle-left icon-left"></i>button')}
-    ${Button('-purple -outline', 'button<i class="fas fa-angle-right icon-right"></i>')}
-    ${Button('-red -outline', '<i class="fas fa-cog"></i>')}
-    ${Button('-white -outline -loading', 'button')}
-    ${Button('-black -outline -loading', 'button')}
-    ${Button('-green -outline -loading', 'button')}`)
-  .add('Gradient', () => `${colors.map(color => Button(`-gradient -${color}`)).join(' ')}
+  .add('Default', () => (<div>
+    <h1>Default button</h1>
+    {Simple()}
+    <SyntaxHighlighter language={'html'} style={darcula} showLineNumbers={true}>
+      {`${ReactDOMServer.renderToStaticMarkup(Simple()).replace(/<\/button> /g, '<\/button>\n')}`}
+    </SyntaxHighlighter>
+  </div>))
+  .add('Outline', () => (<div>
+    <h1>Default button</h1>
+    {Simple(true)}
+    <SyntaxHighlighter language={'html'} style={darcula} showLineNumbers={true}>
+      {`${ReactDOMServer.renderToStaticMarkup(Simple(true)).replace(/<\/button> /g, '<\/button>\n')}`}
+    </SyntaxHighlighter>
+  </div>))
+  .add('Gradient', () => `${colors.map(color => Button(`-gradient -${color}`))}
     ${Button('-blue -gradient', '<i class="fas fa-angle-left icon-left"></i>button')}
     ${Button('-purple -gradient', 'button<i class="fas fa-angle-right icon-right"></i>')}
     ${Button('-red -gradient', '<i class="fas fa-cog"></i>')}
@@ -40,27 +49,27 @@ storiesOf('Simple', module)
     ${Button('-green -gradient -loading', 'button')}`)
 
 storiesOf('Pill', module)
-  .add('Default', () => `${colors.map(color => Button(`-pill -${color}`)).join(' ')}
+  .add('Default', () => `${colors.map(color => Button(`-pill -${color}`))}
     ${Button('-blue -pill', '<i class="fas fa-angle-left icon-left"></i>button')}
     ${Button('-purple -pill', 'button<i class="fas fa-angle-right icon-right"></i>')}
     ${Button('-red -pill', '<i class="fas fa-cog"></i>')}
     ${Button('-white -pill -loading', 'button')}
     ${Button('-black -pill -loading', 'button')}
     ${Button('-green -pill -loading', 'button')}`)
-  .add('Outline', () => `${colors.map(color => Button(`-pill -outline -${color}`)).join(' ')}
+  .add('Outline', () => `${colors.map(color => Button(`-pill -outline -${color}`))}
     ${Button('-blue -pill -outline', '<i class="fas fa-angle-left icon-left"></i>button')}
     ${Button('-purple -pill -outline', 'button<i class="fas fa-angle-right icon-right"></i>')}
     ${Button('-red -pill -outline', '<i class="fas fa-cog"></i>')}
     ${Button('-white -pill -outline -loading', 'button')}
     ${Button('-black -pill -outline -loading', 'button')}
     ${Button('-green -pill -outline -loading', 'button')}`)
-  .add('Gradient', () => `${colors.map(color => Button(`-pill -${color}`)).join(' ')}
+  .add('Gradient', () => `${colors.map(color => Button(`-pill -${color}`))}
     ${Button('-blue -pill -gradient', '<i class="fas fa-angle-left icon-left"></i>button')}
     ${Button('-purple -pill -gradient', 'button<i class="fas fa-angle-right icon-right"></i>')}
     ${Button('-red -pill -gradient', '<i class="fas fa-cog"></i>')}
     ${Button('-white -pill -gradient -loading', 'button')}
     ${Button('-black -pill -gradient -loading', 'button')}
-    ${Button('-green -pill -gradient -loading', 'button')}`)
+    ${Button('-green -pill -gradient -loading', 'button')}`);
 
 storiesOf('Rounded', module)
   .add('Default', () => `${colors.map(color => Button(`-rounded -${color}`)).join(' ')}
