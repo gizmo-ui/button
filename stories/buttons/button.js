e60da78c83e810e9e3deba02ae824a5ca42fc866
Jalal azimi <jalalazimi@gmail.com>
2019-06-17 23:33:37 +0100

    fix(storybook): update demo

--- stories/buttons/button.js
+++ stories/buttons/button.js
@@ -1,5 +1,22 @@
-import renderHTML from "react-render-html";
+import React,{Fragment} from 'react';
+import classnames from "classnames";
 
-const Button = (className, children = "button") => renderHTML(` <button class="button ${className}">${children}</button> `);
+const colors = ['blue', 'purple', 'red', 'orange', 'green', 'white', 'black'];
+
+const Button = (className, children = "button") => <button className={classnames('button',className)}>{children}</button>;
+
+export const ButtonList = ({outline = false, gradient = false, type = ""} = {}) => {
+  const buttonStyle = classnames({'-outline': outline}, {'-gradient': gradient}, {[`-${type}`]: type});
+
+  return [
+    ...colors.map(color => Button(classnames(`-${color}`, buttonStyle))),
+    Button(classnames('-blue', buttonStyle), <Fragment><i className="fas fa-angle-left icon-left"></i>button</Fragment>),
+    Button(classnames('-purple', buttonStyle), <Fragment>button<i className="fas fa-angle-right icon-right"></i></Fragment>),
+    Button(classnames('-white', buttonStyle, '-loading'), 'button'),
+    Button(classnames('-black', buttonStyle, '-loading'), 'button'),
+    Button(classnames('-green', buttonStyle, '-loading'), 'button'),
+    Button(classnames('-green', buttonStyle, '-icon'), <i className="fas fa-align-justify"></i>)
+  ]
+};
 
 export default Button;
--- stories/buttons/demo.js
+++ stories/buttons/demo.js
@@ -7,7 +7,7 @@ const Demo = (src) => (
   <div>
     {src}
     <SyntaxHighlighter language={'html'} style={darcula} showLineNumbers={true}>
-      {`${ReactDOMServer.renderToStaticMarkup(src).replace(/<\/button> /g, '<\/button>\n')}`}
+      {`${ReactDOMServer.renderToStaticMarkup(src).replace(/<\/button>/g, '<\/button>\n').replace(/<\/div>/g, '<\/div>\n').replace(/<div class="grouped">/g, '<div class="grouped">\n')}`}
     </SyntaxHighlighter>
   </div>);
 
